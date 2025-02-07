5f8ece559a4d1309db98910bc4c19e363ec3dcbc
EConrad00 <emil.conrad@campusnykoping.se>
2025-02-07 12:24:10 +0100

    Can now add, subtract and remove from shoppingcart

--- WebShopFood/Methods/Menus.cs
+++ WebShopFood/Methods/Menus.cs
@@ -20,12 +20,12 @@ namespace WebShopFood.Methods
                     Console.WriteLine("Please input your username:");
                     var usernameAttempt = Console.ReadLine();
                     var userId = (from Costumer in db.Costumers
-                                     where Costumer.Id == usernameAttempt
-                                     select Costumer).FirstOrDefault();
+                                  where Costumer.Id == usernameAttempt
+                                  select Costumer).FirstOrDefault();
                     //var chosenList = (from Product in db.Products
                     //                  where Product.Chosen == true
                     //                  select Product).ToList();
-                    if (userId != null && userId.Id == usernameAttempt) 
+                    if (userId != null && userId.Id == usernameAttempt)
                     {
                         Console.WriteLine("Please input your password:");
                         var userPasswordAttempt = Console.ReadLine();
@@ -56,50 +56,87 @@ namespace WebShopFood.Methods
                                         }
                                         break;
                                     case 'a':
-                                        Models.ShoppingItem shoppingItem = new Models.ShoppingItem()
+                                        var shoppingItemsCheck = (from ShoppingItem in db.ShoppingItems
+                                                                  where ShoppingItem.ShoppingCartId == userId.ShoppingCartId && ShoppingItem.ProductId == ChosenGet()[0].Id
+                                                                  select ShoppingItem).ToList();
+
+                                        if (shoppingItemsCheck.Count() == 0)
                                         {
-                                            Quantity = 1,
-                                            Price = ChosenGet()[0].Cost,
-                                            ShoppingCartId = userId.ShoppingCartId,
-                                            ProductId = ChosenGet()[0].Id
-                                        };
-                                        db.ShoppingItems.Add(shoppingItem);
-                                        db.SaveChanges();
+                                            Models.ShoppingItem shoppingItem = new Models.ShoppingItem()
+                                            {
+                                                Quantity = 1,
+                                                Price = ChosenGet()[0].Cost,
+                                                ShoppingCartId = userId.ShoppingCartId,
+                                                ProductId = ChosenGet()[0].Id
+                                            };
+                                            db.ShoppingItems.Add(shoppingItem);
+                                            db.SaveChanges();
+                                        }
+                                        else
+                                        {
+                                            shoppingItemsCheck[0].Quantity = shoppingItemsCheck[0].Quantity + 1;
+                                            db.SaveChanges();
+                                        }
                                         Console.WriteLine("Added one of offer 1 to your shoppingcart!");
                                         Console.ReadKey(true);
                                         break;
                                     case 'b':
-                                        Models.ShoppingItem shoppingItem2 = new Models.ShoppingItem()
+                                        var shoppingItemsCheck2 = (from ShoppingItem in db.ShoppingItems
+                                                                   where ShoppingItem.ShoppingCartId == userId.ShoppingCartId && ShoppingItem.ProductId == ChosenGet()[1].Id
+                                                                   select ShoppingItem).ToList();
+
+                                        if (shoppingItemsCheck2.Count() == 0)
                                         {
-                                            Quantity = 1,
-                                            Price = ChosenGet()[1].Cost,
-                                            ShoppingCartId = userId.ShoppingCartId,
-                                            ProductId = ChosenGet()[1].Id
-                                        };
-                                        db.ShoppingItems.Add(shoppingItem2);
-                                        db.SaveChanges();
+                                            Models.ShoppingItem shoppingItem = new Models.ShoppingItem()
+                                            {
+                                                Quantity = 1,
+                                                Price = ChosenGet()[1].Cost,
+                                                ShoppingCartId = userId.ShoppingCartId,
+                                                ProductId = ChosenGet()[1].Id
+                                            };
+                                            db.ShoppingItems.Add(shoppingItem);
+                                            db.SaveChanges();
+                                        }
+                                        else
+                                        {
+                                            shoppingItemsCheck2[0].Quantity = shoppingItemsCheck2[0].Quantity + 1;
+                                            db.SaveChanges();
+                                        }
                                         Console.WriteLine("Added one of offer 2 to your shoppingcart!");
                                         Console.ReadKey(true);
                                         break;
                                     case 'c':
-                                        Models.ShoppingItem shoppingItem3 = new Models.ShoppingItem()
+
+                                        var shoppingItemsCheck3 = (from ShoppingItem in db.ShoppingItems
+                                                                   where ShoppingItem.ShoppingCartId == userId.ShoppingCartId && ShoppingItem.ProductId == ChosenGet()[2].Id
+                                                                   select ShoppingItem).ToList();
+
+                                        if (shoppingItemsCheck3.Count() == 0)
                                         {
-                                            Quantity = 1,
-                                            Price = ChosenGet()[2].Cost,
-                                            ShoppingCartId = userId.ShoppingCartId,
-                                            ProductId = ChosenGet()[2].Id
-                                        };
-                                        db.ShoppingItems.Add(shoppingItem3);
-                                        db.SaveChanges();
+                                            Models.ShoppingItem shoppingItem = new Models.ShoppingItem()
+                                            {
+                                                Quantity = 1,
+                                                Price = ChosenGet()[2].Cost,
+                                                ShoppingCartId = userId.ShoppingCartId,
+                                                ProductId = ChosenGet()[2].Id
+                                            };
+                                            db.ShoppingItems.Add(shoppingItem);
+                                            db.SaveChanges();
+                                        }
+                                        else
+                                        {
+                                            shoppingItemsCheck3[0].Quantity = shoppingItemsCheck3[0].Quantity + 1;
+                                            db.SaveChanges();
+                                        }
                                         Console.WriteLine("Added one of offer 3 to your shoppingcart!");
                                         Console.ReadKey(true);
                                         break;
                                     case 'q':
                                         Console.WriteLine($"We hope to see you soon again {userId.Name}");
                                         return;
-                                } 
+                                }
                             }
-                            
+
                             //return;
                         }
                         else
@@ -117,7 +154,7 @@ namespace WebShopFood.Methods
 
             }
         }
-        public static void StartMenu(Costumer user, List<Product> chosenProducts) 
+        public static void StartMenu(Costumer user, List<Product> chosenProducts)
         {
             Console.Clear();
             List<string> topText = new List<string> { "# Food store #" };
@@ -145,7 +182,7 @@ namespace WebShopFood.Methods
                 windowTop6.Draw();
 
             }
-            else 
+            else
             {
                 List<string> topText6 = new List<string> { "1. Start menu", "2. Shoppen", "3. Shoppingcart" };
                 var windowTop6 = new Window("Menu options", 77, 0, topText6);
@@ -165,7 +202,7 @@ namespace WebShopFood.Methods
                 List<string> topText = new List<string> { "# Food store #" };
                 var windowTop = new Window("", 42, 0, topText);
                 windowTop.Draw();
-                List<string> topText3 = new List<string> { "1. Products", "2. Categories", "3. Users", "4. Producers", "5. Statistics" };
+                List<string> topText3 = new List<string> { "1. Products", "2. Categories", "3. Users", "4. Producers", "5. Statistics", "Q is return" };
                 var windowTop3 = new Window("Admin funktions", 7, 8, topText3);
                 windowTop3.Draw();
                 ConsoleKeyInfo key = Console.ReadKey(true);
@@ -193,9 +230,9 @@ namespace WebShopFood.Methods
                                     break;
                                 case '4':
                                     return;
-                            } 
+                            }
                         }
-                        //break;
+                    //break;
                     case '2':
                         while (true)
                         {
@@ -240,11 +277,10 @@ namespace WebShopFood.Methods
                                     Methods.Admin.DeleteCostumer();
                                     break;
                                 case '4':
-                                    //Console.WriteLine("Leaving");
                                     return;
                             }
                         }
-                        //break;
+                    //break;
                     case '4':
                         while (true)
                         {
@@ -274,7 +310,7 @@ namespace WebShopFood.Methods
                     case 'q':
                         Menus.StartMenu(user, ChosenGet());
                         return;
-                } 
+                }
             }
         }
 
@@ -298,7 +334,7 @@ namespace WebShopFood.Methods
                                          where ShoppingItem.ShoppingCartId == costumer.ShoppingCartId
                                          select ShoppingItem).ToList();
                 List<string> shoppingItemNamesNoDups = new();
-                foreach (var item in shoppingItemsList) 
+                foreach (var item in shoppingItemsList)
                 {
                     var shoppingItemNames = (from Product in db.Products
                                              where item.ProductId == Product.Id
@@ -306,12 +342,67 @@ namespace WebShopFood.Methods
                     shoppingItemNamesNoDups.Add(shoppingItemNames);
                 }
                 shoppingItemNamesNoDups = shoppingItemNamesNoDups.Distinct().ToList();
+                int i = 0;
+                Console.WriteLine("Your shoppingcart\n");
                 foreach (var itemName in shoppingItemNamesNoDups)
                 {
-                    Console.WriteLine(itemName);
+                    Console.WriteLine($"{itemName} Amount: {shoppingItemsList[i].Quantity}");
+                    i++;
 
                 }
-                Console.ReadKey(true);
+                Console.WriteLine("|Press 1 to add or subtract from chosen products| Press 2 to remove a product completely form the shoppingcart| Press 3 to return  | Press x to checkout|");
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine("Write the name of the product you want to alter the amount of");
+                        string productToAlterAmount = Console.ReadLine();
+                        var productId = Methods.Admin.FindProductId(db, productToAlterAmount);
+                        while (true)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(productToAlterAmount);
+                            Console.WriteLine("Press 1 to add| Press 2 to subtract| Press 3 to return");
+                            ConsoleKeyInfo key2 = Console.ReadKey(true);
+                            switch (key2.KeyChar)
+                            {
+                                case '1':
+                                    var shoppingItemsCheck3 = (from ShoppingItem in db.ShoppingItems
+                                                               where ShoppingItem.ShoppingCartId == costumer.ShoppingCartId && ShoppingItem.ProductId == productId
+                                                               select ShoppingItem).ToList();
+                                    shoppingItemsCheck3[0].Quantity = shoppingItemsCheck3[0].Quantity + 1;
+                                    db.SaveChanges();
+                                    break;
+                                case '2':
+                                    var shoppingItemsCheck4 = (from ShoppingItem in db.ShoppingItems
+                                                               where ShoppingItem.ShoppingCartId == costumer.ShoppingCartId && ShoppingItem.ProductId == productId
+                                                               select ShoppingItem).ToList();
+                                    shoppingItemsCheck4[0].Quantity = shoppingItemsCheck4[0].Quantity - 1;
+                                    db.SaveChanges();
+                                    break;
+                                case '3':
+                                    return;
+                            }
+                        }
+                        //break;
+                    case '2':
+                        Console.WriteLine("Write the name of the product you want to remove");
+                        productToAlterAmount = Console.ReadLine();
+                        productId = Methods.Admin.FindProductId(db, productToAlterAmount);
+
+                        var shoppingItemsCheck5 = (from ShoppingItem in db.ShoppingItems
+                                                   where ShoppingItem.ShoppingCartId == costumer.ShoppingCartId && ShoppingItem.ProductId == productId
+                                                   select ShoppingItem).SingleOrDefault();
+                        db.Remove(shoppingItemsCheck5);
+                        db.SaveChanges();
+                        break;
+                    case '3':
+                        break;
+                    case 'r':
+                        return;
+
+                        //Console.ReadKey(true);
+                }
             }
         }
     }
