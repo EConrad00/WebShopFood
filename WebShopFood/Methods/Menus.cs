855dde7e497fb7aa45160c1fc13a1e9564ab800d
EConrad00 <emil.conrad@campusnykoping.se>
2025-02-07 05:43:40 +0100

    Added more menus

--- WebShopFood/Methods/Menus.cs
+++ WebShopFood/Methods/Menus.cs
@@ -19,13 +19,12 @@ namespace WebShopFood.Methods
                     Console.Clear();
                     Console.WriteLine("Please input your username:");
                     var usernameAttempt = Console.ReadLine();
-                    //foreach (var costumer in db.Costumers) 
-                    //{
-
-                    //}
                     var userId = (from Costumer in db.Costumers
                                      where Costumer.Id == usernameAttempt
                                      select Costumer).FirstOrDefault();
+                    //var chosenList = (from Product in db.Products
+                    //                  where Product.Chosen == true
+                    //                  select Product).ToList();
                     if (userId != null && userId.Id == usernameAttempt) 
                     {
                         Console.WriteLine("Please input your password:");
@@ -34,7 +33,7 @@ namespace WebShopFood.Methods
                         {
                             Console.WriteLine($"Welcome back {userId.Name}!");
                             Console.ReadKey(true);
-                            Menus.StartMenu(userId);
+                            Menus.StartMenu(userId,ChosenGet());
                             return;
                         }
                         else
@@ -52,16 +51,64 @@ namespace WebShopFood.Methods
 
             }
         }
-        public static void StartMenu(Costumer user) 
+        public static void StartMenu(Costumer user, List<Product> chosenProducts) 
         {
             Console.Clear();
             List<string> topText = new List<string> { "# Food store #" };
-            var windowTop = new Window("", 25, 0, topText);
+            var windowTop = new Window("", 42, 0, topText);
             windowTop.Draw();
             List<string> topText2 = new List<string> { $"{user.Name}" };
-            var windowTop2 = new Window("", 2, 0, topText2);
+            var windowTop2 = new Window("", 7, 0, topText2);
             windowTop2.Draw();
+            var product1 = chosenProducts[0];
+            var product2 = chosenProducts[1];
+            var product3 = chosenProducts[2];
+            List<string> topText3 = new List<string> { $"{product1.Name}", $"{product1.Description}", $"Pris:{product1.Cost}", "Press A to add to shoppingcart" };
+            var windowTop3 = new Window("Offer 1", 2, 8, topText3);
+            windowTop3.Draw();
+            List<string> topText4 = new List<string> { $"{product2.Name}", $"{product2.Description}", $"Pris:{product2.Cost}", "Press B to add to shoppingcart" };
+            var windowTop4 = new Window("Offer 2", 37, 8, topText4);
+            windowTop4.Draw();
+            List<string> topText5 = new List<string> { $"{product3.Name}", $"{product3.Description}", $"Pris:{product3.Cost}", "Press C to add to shoppingcart" };
+            var windowTop5 = new Window("Offer 3", 72, 8, topText5);
+            windowTop5.Draw();
+            if (user.Admin == true)
+            {
+                List<string> topText6 = new List<string> { "1. Start menu", "2. Shoppen", "3. Shoppingcart", "4. Admin page" };
+                var windowTop6 = new Window("Menu options", 77, 0, topText6);
+                windowTop6.Draw();
+
+            }
+            else 
+            {
+                List<string> topText6 = new List<string> { "1. Start menu", "2. Shoppen", "3. Shoppingcart" };
+                var windowTop6 = new Window("Menu options", 77, 0, topText6);
+                windowTop6.Draw();
+            }
             Console.ReadKey(true);
+        }
+
+        public static void AdminMenu(Costumer user)
+        {
+            Console.Clear();
+            List<string> topText2 = new List<string> { $"{user.Name}" };
+            var windowTop2 = new Window("", 7, 0, topText2);
+            windowTop2.Draw();
+            List<string> topText = new List<string> { "# Food store #" };
+            var windowTop = new Window("", 42, 0, topText);
+            windowTop.Draw();
+
+        }
+
+        public static List<Product> ChosenGet()
+        {
+            using (var db = new WebShopFoodContext())
+            {
+                var chosenList = (from Product in db.Products
+                                  where Product.Chosen == true
+                                  select Product).ToList();
+                return chosenList;
+            }
         }
     }
 }
